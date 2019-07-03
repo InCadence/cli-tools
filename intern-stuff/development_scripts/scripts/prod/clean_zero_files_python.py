7319725ea5c12e51b714b821afabadc1d9c60985
michaelslu <michaelslu@outlook.com>
2019-07-03 11:59:36 -0400

    finished conversion, pushed to master branch

--- intern-stuff/development_scripts/scripts/prod/clean_zero_files_python.py
+++ intern-stuff/development_scripts/scripts/prod/clean_zero_files_python.py
@@ -3,16 +3,23 @@
 #Input the path you wish to clean
 #This will recursively find all files
 # of 0 size and remove them
+
 import sys
 import os
 
-#gets path
-path = input("Enter the path: ")
+#main method
+def main():
 
-#loops through the dir and cleans files of 0 size
-for root, dirs, files in os.walk(path):
-    for name in files:
-        filename = os.path.join(root,name)
-        if os.stat(filename).st_size == 0:
-            print("removing ", filename)
-            os.remove(filename)
+    #gets path from user
+    path = str(input("Enter the path surrounded by quotes (\"p/a/t/h...\"): "))
+
+    #loops through the directory and removes any files of size 0
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            filename = os.path.join(root,name)
+            if os.stat(filename).st_size == 0:
+                print("removing ", filename)
+                os.remove(filename)
+
+if __name__ == "__main__":
+    main()
