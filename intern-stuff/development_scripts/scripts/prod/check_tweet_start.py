dc31c6aeef0be14262cd1346794e70bc2a87fe4d
michaelslu <michaelslu@outlook.com>
2019-07-08 11:30:46 -0400

    still working

--- intern-stuff/development_scripts/scripts/prod/check_tweet_start.py
+++ intern-stuff/development_scripts/scripts/prod/check_tweet_start.py
@@ -11,7 +11,7 @@ with open("addedTweets.json") as f:
 try:
     parsed_json = json.loads(json_string)
     formatted_json = json.dumps(parsed_json, indent=4, sort_keys= True)
-    with open("addedTweets.json") as f:
+    with open("addedTweets.json", "w") as f:
         f.write(formatted_json)
 except Exception as e:
     print(repr(e))
