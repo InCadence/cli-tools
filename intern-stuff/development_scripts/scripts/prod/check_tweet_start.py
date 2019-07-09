e8591bbfadf2cdd7ac4badfe4ad033b55914d36e
michaelslu <michaelslu@outlook.com>
2019-07-09 10:42:24 -0400

    finishing up check tweet start, added user input so the user can specify what file they want formatted/validated.

--- intern-stuff/development_scripts/scripts/prod/check_tweet_start.py
+++ intern-stuff/development_scripts/scripts/prod/check_tweet_start.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 
+# This script goes through the addedTweets.json 
+# files and validates and formats the data.
+# Enter the file name surrounded by quotes ("......")
+
 import json
 
 json_string = None
+jsonfile = str(input("Please enter the file name: "))
+while True:
+    with open(jsonfile) as f:
+        json_string = f.read()
+    try:
+        parsed_json = json.loads(json_string)
+        formatted_json = json.dumps(parsed_json, indent=4, sort_keys= True)
+        with open(jsonfile, "w") as f:
+            f.write(formatted_json)
+    except Exception as e:
+        print(repr(e))
+        break
 
-with open("addedTweets.json") as f:
-    json_string = f.read()
-try:
-    parsed_json = json.loads(json_string)
-    formatted_json = json.dumps(parsed_json, indent=4, sort_keys= True)
-    with open("addedTweets.json", "w") as f:
-        f.write(formatted_json)
-except Exception as e:
-    print(repr(e))
 
 
