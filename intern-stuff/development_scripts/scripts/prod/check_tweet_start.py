9b5255635ff7b5bd70c7ef1e2174cf0252b92c7d
michaelslu <michaelslu@outlook.com>
2019-07-09 11:06:58 -0400

    added main method

--- intern-stuff/development_scripts/scripts/prod/check_tweet_start.py
+++ intern-stuff/development_scripts/scripts/prod/check_tweet_start.py
@@ -5,20 +5,25 @@
 # Enter the file name surrounded by quotes ("......")
 
 import json
+def main():
 
-json_string = None
-jsonfile = str(input("Please enter the file name: "))
-while True:
-    with open(jsonfile) as f:
-        json_string = f.read()
-    try:
-        parsed_json = json.loads(json_string)
-        formatted_json = json.dumps(parsed_json, indent=4, sort_keys= True)
-        with open(jsonfile, "w") as f:
-            f.write(formatted_json)
-    except Exception as e:
-        print(repr(e))
-        break
+    json_string = None
+    jsonfile = str(input("Please enter the file name: "))
+    while True:
+        with open(jsonfile) as f:
+            json_string = f.read()
+        try:
+            parsed_json = json.loads(json_string)
+            formatted_json = json.dumps(parsed_json, indent=4, sort_keys= True)
+            with open(jsonfile, "w") as f:
+                f.write(formatted_json)
+            print("looks good")
+        except Exception as e:
+            print(repr(e))
+            break
+
+if __name__ == "__main__":
+    main()
 
 
 
