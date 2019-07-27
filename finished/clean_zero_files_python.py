0e6c8138d03c83b62ab438f89ab654b182ec16eb
michaelslu <michaelslu@outlook.com>
2019-07-27 19:44:02 -0400

    Finished zero byte file cleaner script

--- finished/clean_zero_files_python.py
+++ finished/clean_zero_files_python.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Input the path you wish to clean
+# Run the script while passing the directory you wish to clean.
 # This will recursively find all files
 # of 0 size and remove them
 
@@ -8,26 +8,23 @@ import sys
 import os
 
 # main method
-def main():
-    """
-    Michael cleans things up
-    """
+def main(): 
 
-    # gets path passed in from the user
+    # passed in from user
     args = sys.argv
     if len(args) != 2:
         # if we got here then they either entered too many args or not enough
         print("You must supply one directory to clean")
 
-        # time to bail
+        # bail
         sys.exit()
     else:
-        # ok, looking good. let's remove some files
+        # remove some files
         path = sys.argv[1]
         print("Cleaning Directory : %s" % path)
 
         # loops through the directory and removes any files of size 0
-        for root, dirs, files in os.walk(path):
+        for root, files in os.walk(path):
             for name in files:
                 filename = os.path.join(root, name)
                 if os.stat(filename).st_size == 0:
